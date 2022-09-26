e3f94e1edaf4551ae9b45e40a25f194a5735b12b
Feras <ferassaleh1980@gmail.com>
2022-09-26 21:07:09 +0200

    js

---
+++ index.js
@@ -0,0 +1,16 @@
+const heading = document.getElementById('heading');
+
+function small() {
+    heading.className = ' ';
+    heading.classList.add('small');
+}
+
+function middle() {
+    heading.className = ' ';
+    heading.classList.add('middle');
+}
+
+function big() {
+    heading.className = ' ';
+    heading.classList.add('big');
+}
